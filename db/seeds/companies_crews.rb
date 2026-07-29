6d579d146885f594b91f084591751c4fe2a542ed
HermanSigue <hermansigue@gmail.com>
2026-07-29 09:05:32 +0700

    chore(seeds): enrich vessel gear and crew data

--- db/seeds/companies_crews.rb
+++ db/seeds/companies_crews.rb
@@ -10,8 +10,15 @@ COMPANIES_CREWS = [
     position: "Engineer", nationality: "Bruneian", date_of_birth: Date.new(1993, 6, 22), approve: true },
   { company_profile: commercial_profile, crew_name: "Rizal bin Kassim", ic_number: "00901234",
     position: "Deckhand", nationality: "Malaysian", date_of_birth: Date.new(1995, 11, 2), approve: false },
+  { company_profile: commercial_profile, crew_name: "Faris bin Jamal", ic_number: "01234567",
+    position: "Navigator", nationality: "Bruneian", date_of_birth: Date.new(1988, 7, 9), approve: true },
+  { company_profile: commercial_profile, crew_name: "Daniel Lim Wei Kiat", ic_number: "01345678",
+    passport_number: "A12345678", position: "Mechanic", nationality: "Malaysian",
+    date_of_birth: Date.new(1991, 12, 18), approve: true },
   { company_profile: small_scale_company_profile, crew_name: "Hafiz bin Matassan", ic_number: "01012345",
-    position: "Deckhand", nationality: "Bruneian", date_of_birth: Date.new(1992, 3, 30), approve: true }
+    position: "Deckhand", nationality: "Bruneian", date_of_birth: Date.new(1992, 3, 30), approve: true },
+  { company_profile: small_scale_company_profile, crew_name: "Azlan bin Salleh", ic_number: "01456789",
+    position: "Deckhand", nationality: "Bruneian", date_of_birth: Date.new(1996, 4, 25), approve: true }
 ].freeze
 
 COMPANIES_CREWS.each do |attrs|
