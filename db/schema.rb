8d691abc2148fe6a0226eca486c1b724c20bb13d
HermanSigue <hermansigue@gmail.com>
2026-08-26 19:02:27 +0700

    feat: add local citizenship flag to nationalities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_11_110000) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_26_103000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pg_trgm"
@@ -412,6 +412,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_11_110000) do
   create_table "nationalities", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
+    t.boolean "is_local_citizenship", default: false, null: false
     t.string "name", null: false
     t.datetime "updated_at", null: false
     t.index ["code"], name: "index_nationalities_on_code"
