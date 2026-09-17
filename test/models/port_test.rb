be03ef8985cc14009bf1089a4dc036ffc75a8d5c
HermanSigue <hermansigue@gmail.com>
2026-09-17 19:59:43 +0700

    chore: refresh port and zone schema annotations

--- test/models/port_test.rb
+++ test/models/port_test.rb
@@ -11,3 +11,22 @@ class PortTest < ActiveSupport::TestCase
     assert_not Port.kept.exists?(port.id)
   end
 end
+
+# == Schema Information
+#
+# Table name: ports
+# Database name: primary
+#
+#  id           :uuid             not null, primary key
+#  discarded_at :datetime
+#  latitude     :decimal(10, 8)
+#  longitude    :decimal(11, 8)
+#  port_name    :string           not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+# Indexes
+#
+#  index_ports_on_discarded_at  (discarded_at)
+#  index_ports_on_port_name     (port_name)
+#
